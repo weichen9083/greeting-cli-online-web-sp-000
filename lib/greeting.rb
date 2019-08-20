a406ad7a77bdd84cbe39b32a666422a8a9c0cfca
wei chen <weichen9083@gmail.com>
2019-08-20 19:50:08 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-  what = name.strip
-  print "Hello #{what}. It's nice to meet you."
+  print "Hello #{name.strip}. It's nice to meet you."
 end 
